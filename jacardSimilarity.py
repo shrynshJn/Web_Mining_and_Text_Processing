fa45ce754a885a8147c9edb3e91191c65e9dd4ea
shrynshJn <shreyanshmacro@gmail.com>
2019-09-19 11:17:11 +0530

    jaccard

--- jacardSimilarity.py
+++ jacardSimilarity.py
@@ -1,6 +1,8 @@
+# expects to term frequency vectors or term conatining vecotr
 def jacardSimilairty(a, b):
-    a = set(a)
-    b = set(b)
-    union = a.union(b)
-    intersection = a.intersection(b)
-    return (intersection/union)
+    intersection = 0
+    union = 0
+    for i in range(len(a)):
+        intersection = (a[i] > 0 and b[i] > 0)
+        union = (a[i] > 0 or b[i] > 0)
+    return intersection/union
