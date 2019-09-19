fcb4cd11b891294f21c41c610fdc615610b11aff
shrynshJn <shreyanshmacro@gmail.com>
2019-09-19 11:45:41 +0530

    golomb

--- encoding.py
+++ encoding.py
@@ -21,7 +21,16 @@ def bin2dec(binary):
 def remainder_encoding(r, b):
     i = math.floor(math.log(b, 2))
     d = 2**(i+1) - b
-    return d
+    if (r >= d):
+        binary = dec2bin(r + d)
+        if (len(binary) < i + 1):
+            binary = "0"*(i + 1 - len(binary)) + binary
+        return binary
+    else: 
+        binary = dec2bin(r)
+        if (len(binary) < i):
+            binary = "0"*(i  - len(binary))  + binary
+        return binary
 
 def elias_gamma_encoding(num):
     binary = dec2bin(num)
@@ -50,9 +59,22 @@ def elias_delta_decoding(delta):
     return (bin2dec(num_binary))
     
 def golomb_encoding(num, b):
-    q = (int)(num/b)
+    q = int(num/b)
     part1 = unary_encoding(q + 1)
     r = num - q*b
-    return part1 + r
-
-print(elias_delta_decoding(elias_detla_encoding(50)))
+    part2 = remainder_encoding(r, b)
+    return part1 + part2
+    
+def golomb_decoding(golomb, b):
+    q = golomb.find("1")
+    i = math.floor(math.log(b,2))
+    d = 2**(i+1) - b
+    rem = golomb[q + 1:]
+    if (len(rem) == i):
+        r = bin2dec(golomb[q + 1:])
+    else:
+        r = bin2dec(golomb[q + 1:]) - d
+    return q*b + r
+for i in range(1, 11):
+    print(golomb_decoding(golomb_encoding(i, 3),3), golomb_decoding(golomb_encoding(i, 10),10))
+    
